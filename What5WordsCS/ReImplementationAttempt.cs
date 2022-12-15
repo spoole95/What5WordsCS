b3408ad0cbd421da4c4014e643a50d2dae7d5478
Steven Poole <Steven.Poole@teamsoftware.com>
2022-12-15 16:33:29 +0000

    Parallel, logging and fix bug

--- What5WordsCS/ReImplementationAttempt.cs
+++ What5WordsCS/ReImplementationAttempt.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Numerics;
 using System.Runtime.Intrinsics.X86;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace What5WordsCS
@@ -15,29 +16,78 @@ namespace What5WordsCS
         private static int[] LetterOrder = new int[26];
         private static List<uint>[] LetterIndex = new List<uint>[26];
 
+        private static List<State> Queue = new List<State>();
+
         private struct Freq
         {
             public int f;
             public int l;
         }
 
+        private struct State
+        {
+            public uint Total;
+            public uint[] Words;
+            public uint MaxLetter;
+            public bool Skipped;
+            public bool End;
+        }
+        //13174
+
         public static void Run()
         {
+            var start = DateTime.Now;
             ReadWords("words_alpha.txt", 5);
 
+            var startAlgo = DateTime.Now;
             var solutions = new List<uint[]>();
-            FindWords(solutions, 0, new uint[5], 0, false);
 
-            Console.WriteLine(solutions.Count + " solutions found");
+            var serial = false;
+            FindWords(solutions, 0, new uint[5], 0, false, !serial);
+
+            if (!serial)
+            {
+                Parallel.ForEach(Queue, new ParallelOptions { },
+                    (state) => FindWords(solutions, state.Total, state.Words, state.MaxLetter, state.Skipped, true));
+            }
+
+            var startOut = DateTime.Now;
+
+            const bool output = true;
+            if (output)
+            {
+                foreach (var solution in solutions)
+                {
+                    Console.WriteLine($"{AllWords[solution[0]]}, {AllWords[solution[1]]}, {AllWords[solution[2]]}, {AllWords[solution[3]]}, {AllWords[solution[4]]}");
+                }
+            }
+
+            var end = DateTime.Now;
+            Console.WriteLine(solutions.Count + " solutions found"); // 538
+            Console.WriteLine($"Total time: {(end - start).TotalMilliseconds}ms ({(end - start).TotalSeconds}s)"); //cpp 33.2548ms (0.332548s), c# standard 1.99s
+            Console.WriteLine($"Read: {(startAlgo - start).TotalMilliseconds}ms"); // 5.6522ms, c# standard 84.03ms
+            Console.WriteLine($"Process: {(startOut - startAlgo).TotalMilliseconds}ms"); // 4.1249ms, c# standard 678.4, c# paraell 260.84ms
+            Console.WriteLine($"Write: {(end - startOut).TotalMilliseconds}ms"); // 23.4777ms, c# standard 1229.4ms
         }
 
-        private static void FindWords(List<uint[]> solutions, uint total, uint[] currentWords, uint maxLetter, bool skipped)
+        private static void FindWords(List<uint[]> solutions, uint total, uint[] currentWords, uint maxLetter, bool skipped, bool force = false)
         {
             var words = currentWords.Count(x => x != 0);
             if (words == 5)
             {
-                Console.WriteLine($"Found solution: {AllWords[currentWords[0]]}, {AllWords[currentWords[1]]}, {AllWords[currentWords[2]]}, {AllWords[currentWords[3]]}, {AllWords[currentWords[4]]}");
                 solutions.Add(currentWords);
+                return;
+            }
+
+            if (!force && words == 1)
+            {
+                Queue.Add(new State
+                {
+                    Total = total,
+                    Words = currentWords,
+                    MaxLetter = maxLetter,
+                    Skipped = skipped
+                });
                 return;
             }
 
@@ -59,13 +109,11 @@ namespace What5WordsCS
                 }
 
                 foreach (uint w in LetterIndex[i])
-                //Parallel.ForEach(LetterIndex[i], new ParallelOptions { MaxDegreeOfParallelism = 5 }, (w) =>
                 {
                     if ((total & w) > 0)
                     {
                         //Letter of word w is used already
                         continue;
-                        //return; // Parallel only
                     }
 
                     //Add this attempt to the list
@@ -76,7 +124,10 @@ namespace What5WordsCS
                     //Recusive to find word that now satisfies all words in currentWords, if none found then this exits
                     FindWords(solutions, total | w, (uint[])currentWords.Clone(), i + 1, skipped);
                 }
-                //);
+
+
+                if (skipped) break;
+                skipped = true; //Skip all future if not found a result on the first pass. Limit
             }
         }
 
